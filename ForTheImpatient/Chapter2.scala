4e078a5453c4836ebde6e971e4d71d8eb5340237
BW L33t <bwhackathon@gmail.com>
2012-08-18 16:31:03 -0400

    Initial project

---
+++ ForTheImpatient/Chapter2.scala
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sultan
+ * Date: 8/16/12
+ * Time: 2:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+def signum(i : Int) : Int = {
+  if (i > 0) 1 else if (i < 0) -1 else 0
+}
+
+//var x =
+println(signum(5))
+println(signum(-4))
+println(signum(0))
+println("done")
+//print signum(-5)
+//print signum(0)
